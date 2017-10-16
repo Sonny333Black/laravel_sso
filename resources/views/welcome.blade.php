75cc5ede835006777cf697c61e54a3072434298d
U-INTERLUTIONS\cschulz <cschulz@ITL-PC-042.interlutions.local>
2017-10-16 17:31:28 +0200

    hoffe es geht

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SSO-IDP
                 </div>
 
                 <div class="links">
